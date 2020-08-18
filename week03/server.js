c9fe50b4bd784dc176e1126929be109a8e020ef8
thomas <zhangfan@thomas-fan.com>
2020-08-18 18:12:00 +0800

    完成制作 dom 树

---
+++ week03/server.js
@@ -0,0 +1,26 @@
+const http = require('http')
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html')
+    res.setHeader('X-Foo', 'bar')
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end(`
+        <html maaa=a>
+            <head>
+                <style>
+                    body div #myid{
+                        width: 100px;
+                        background-color:#ff5000;
+                    }
+                    
+                    body div img {
+                        width: 30px;
+                    }
+                </style>
+            </head>
+            <body>
+                <div>123</div>
+            </body>
+        </html>
+    `)
+}).listen(8088)
+console.log("server started")
